21290e7331814a9bdeac49211c26d6b6210bd43f
Diego <diegoartiles13@gmail.com>
2021-04-26 23:53:07 -0300

    feat (props): remove prop index from Icon in CategoriesList

--- src/components/CategoriesList/Index.js
+++ src/components/CategoriesList/Index.js
@@ -2,8 +2,8 @@ import { Icon } from "../Icon/Index";
 
 export const CategoriesList = ({iconList = []}) => <section className="categories-list">
     { 
-        iconList.map((icon, index) => <div className="categories-list__item">
-                <Icon data={icon} index={index}/>
+        iconList.map((icon) => <div className="categories-list__item">
+                <Icon data={icon}/>
             </div>
         ) 
     }
