540b339573db5adc500ced3d712af7828e72af94
Diego <diegoartiles13@gmail.com>
2021-04-27 21:54:22 -0300

    fix (a): fix invalid href attribute

--- src/components/StoreLocation/Index.js
+++ src/components/StoreLocation/Index.js
@@ -4,7 +4,7 @@ export const StoreLocation = ({location = 'Caracas, Ajuro'}) => <section classNa
 <h2 className="store-location__text">
   <b className="store-location__text-bold">Seleciona tu tienda</b>
 </h2>
-<a href="#" className="store-location__link">
+<a href="#location" name="location" className="store-location__link">
   <img src={LocationIcon} alt="location icon" className="store-location__icon-location" />
   <span className="store-location__address">{location}</span>
 </a>
